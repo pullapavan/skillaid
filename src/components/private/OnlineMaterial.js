13054246e73b13b75fc3229050376f08248529e1
Pulla pavan Kumar <pulla.kumar@thoughtspot.com>
2022-07-07 23:01:11 +0530

    remove material signup

--- src/components/private/OnlineMaterial.js
+++ src/components/private/OnlineMaterial.js
@@ -3,13 +3,13 @@ import Register from '../../pages/account/Register'
 import Materials from '../../pages/courses/Materials'
 
 export default function OnlineMaterial() {
-    let flag = localStorage.getItem("onlinematerialsignup")
-    if (flag) {
+    // let flag = localStorage.getItem("onlinematerialsignup")
+    // if (flag) {
         return <Materials />
-    }
-    return (
-        <>
-            <Register redirectTo="materials" />
-        </>
-    )
+    // }
+    // return (
+    //     <>
+    //         <Register redirectTo="materials" />
+    //     </>
+    // )
 }
